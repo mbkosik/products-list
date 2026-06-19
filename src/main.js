24a29dba36835c19f47a77025ce81cf62c5f224e
Mateusz Kosik <kosikmateusz@wp.pl>
2026-06-19 19:38:09 +0200

    feat: add fetchData function and log reponse in the browser console

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
-const apiUrl = import.meta.env.VITE_API_URL
+const apiUrl = import.meta.env.VITE_API_URL;
 
-console.log(apiUrl)
+if (!apiUrl) {
+  throw new Error('VITE_API_URL is not defined');
+}
+
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+fetchData(apiUrl);
